ca5d3937516051e16fe0946aa6ba953d14e69304
Daniel Hofmann <daniel.hofmann01@gmx.at>
2021-09-04 13:19:47 +0200

    start mail service

---
+++ SchülerHelfenSchüler/Controllers/MailController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using SchülerHelfenSchüler.Models;
+
+namespace SchülerHelfenSchüler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        private SchülerHelfenSchülerContext context;
+
+        public MailController(SchülerHelfenSchülerContext context) {
+            this.context = context;
+        }
+
+        public ActionResult SendMail([FromBody] RequestMessage message) {
+            string fromAdress = context.Users.Where(x => x.Id == message.FromId).First().Email;
+            string toAdress = context.Users.Where(x => x.Id == message.ToId).First().Email;
+
+            //Delete once tested
+            toAdress = "20152081@atn.ac.at";
+
+            MailMessage mailMessage = new MailMessage(fromAdress, toAdress, message.Subject, message.Body);
+
+
+
+            return BadRequest();
+        }
+    }
+}
---
+++ SchülerHelfenSchüler/Models/RequestMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchülerHelfenSchüler.Models {
+    public class RequestMessage {
+        public string Body { get; set; }
+        public string FromId { get; set; }
+        public string ToId { get; set; }
+        public string Subject { get; set; }
+    }
+}
